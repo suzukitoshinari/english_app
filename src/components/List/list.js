99c39e4f499a9d99fe66a5e17f101a4ac47ae408
suzukitoshinari <ay658mymt@gmail.com>
2021-01-13 14:50:44 +0900

    変更

--- src/components/List/list.js
+++ src/components/List/list.js
@@ -1,51 +1,50 @@
 import React, { useState }　from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import{ TextField, List, ListItem, Checkbox } from '@material-ui/core';
+import { TextField, List, ListItem, Checkbox } from '@material-ui/core';
+import { v4 as uuidv4 } from 'uuid';
 
 const useStyles = makeStyles({
   root: {
-    width: '100%',
-  },
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: 'white',
-    border: '2px solid #000',
-    boxShadow: 5,
-    padding: 3
-  },
-  btn: {
-    width: '100%'
-  },
-  main: {
-    width: '70%',
-    height: '70%'
-  },
-  list: {
-    width: '80%',
-    padding: 0,
-    backgroundColor: 'white',
-    border: '1px solid black',
-  },
-  container: {
-    display: 'flex',
-  },
-  item: {
-    borderBottom: '1px solid black',
-    height: 40
-  }
+        width: '100%',
+      },
+      modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      paper: {
+        backgroundColor: 'white',
+        border: '2px solid #000',
+        boxShadow: 5,
+        padding: 3
+      },
+      btn: {
+        width: '100%'
+      },
+      main: {
+        width: '70%',
+        height: '70%'
+      },
+      list: {
+        width: '80%',
+        padding: 0,
+        backgroundColor: 'white',
+        border: '1px solid black',
+      },
+      container: {
+        display: 'flex',
+      },
+      item: {
+        borderBottom: '1px solid black',
+        height: 40
+      }
 });
-// const id = lists.length;
 
 const StickyTable = () => {
   const classes = useStyles();
 
-  const [words, setWord] = useState([]),
+  const [items, setItems] = useState([]),
         [newWord, setNewWord] = useState(''),
-        [meanings, setMeaning] = useState([]),
         [newMeaning, setNewMeaning] = useState('');
 
   const handleNewWord = (e) => {
@@ -55,78 +54,37 @@ const StickyTable = () => {
     setNewMeaning(e.target.value);
   };
 
-  const koko = () => {
-    if(words && newWord === '' || newMeaning === '') {
+  const onClickAdd = () => {
+    if (newWord === '' || newMeaning === '') {
       alert('単語と意味の両方を入力してください。');
-      setNewWord('');
-      setNewMeaning('');
       return;
     } else if(newWord.match(/[^A-Za-z0-9]+/)) {
       alert('英語で入力してください。');
-      setNewWord('');
-      setNewMeaning('');
       return;
     }  else {
       handleAddWord();
-      handleAddMeaning();
     }
   }
 
-  const onClickAdd = () => {
-  　koko();
-  }
-
   const handleAddWord = () => {
-    // e.preventDefault()
-    // const id = words.length ? words[words.length - 1].id + 1 : 0;
-    setWord([...words, {
-      id: Date.now(), item: newWord, isCompleted: false
-    }])
+    setItems([...items, {
+      id: uuidv4(), word: newWord, meaning: newMeaning, isCompleted: false
+    }]);
     setNewWord('');
-  }
-
-  const handleAddMeaning = () => {
-    // e.preventDefault()
-    // const id = meanings.length ? meanings[meanings.length - 1].id + 1 : 0;
-    setMeaning([...meanings, {
-      id: Date.now(), content: newMeaning, isCompleted: false
-    }])
     setNewMeaning('');
   }
 
-  const handleDeleteWord = (id) => {
-    // const id = words.length ? words[words.length - 1].id + 1 : 0;
-    setWord(words.filter((word) => word.id !== id));
-  }
-
-  const handleDeleteMeaning = (id) => {
-    setMeaning(meanings.filter((meaning) => meaning.id !== id));
-  }
-
   const onClickDelete = () => {
-      handleDeleteWord();
-      handleDeleteMeaning();
+    setItems(items.filter((item) => !item.isCompleted));
   }
 
-  const toggleDeleteWord = (id) => {
-    setWord(words.map(word => {
-        if (word.id === id) {
-          return {...word, isCompleted: !word.isCompleted}
+  const toggleCompleted = (id) => {
+    setItems(items.map(item => {
+        if (item.id === id) {
+          return {...item, isCompleted: !item.isCompleted}
         }
-        return word
-      }))
-  }
-
-  const toggleDeleteMeaning = (id) => {
-    setMeaning(meanings.filter(meaning => {
-        if (meaning.id === id) meaning.isCompleted = !meaning.isCompleted
-        return meaning
-    }))
-  }
-
-  const toggleDelete = (id) => {
-    toggleDeleteWord(id);
-    toggleDeleteMeaning();
+        return item
+      }));
   }
 
   return (
@@ -138,28 +96,28 @@ const StickyTable = () => {
           <button type='button' onClick={onClickAdd}>
             登録する
           </button>
-          <button onClick={onClickDelete}>
+          <button type='button' onClick={onClickDelete}>
             削除
           </button>
         </form>
         <div className={classes.container}>
           <List component='ol' className={classes.list}>
-            {words.map((word,  id) => (
-              <ListItem key={id} component='li' className={classes.item}>
+            {items.map((item) => (
+              <ListItem key={item.id} component='li' className={classes.item}>
                 <Checkbox
                   color="primary"
                   inputProps={{ 'aria-label': 'secondary checkbox' }}
-                  onClick={toggleDelete}
-                  defaultChecked={word.isCompleted}
+                  onClick={e => toggleCompleted(item.id)}
+                  value={item.isCompleted}
                 />
-                {word.item}
+                {item.word}
               </ListItem>
             ))}
           </List>
           <List component='ol' className={classes.list}>
-            {meanings.map((meaning, id) => (
-              <ListItem key={id} component='li' className={classes.item}>
-                {meaning.content}
+            {items.map((item) => (
+              <ListItem key={item.id} component='li' className={classes.item}>
+                {item.meaning}
               </ListItem>
             ))}
           </List>
@@ -173,3 +131,9 @@ const StickyTable = () => {
 }
 
 export default StickyTable;
+
+
+
+
+
+
